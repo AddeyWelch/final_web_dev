45d91d03dd3f005b68c7ae32aa0b889c5de65758
Addey Welch <awelch@basecampcodingacademy.org>
2017-03-06 13:46:09 -0600

    added rental javascript file

---
+++ rental.js
@@ -0,0 +1,13 @@
+var calc = {
+  total: 0,
+  inventory: {"Castle Bouncer": 100;
+    "Sports Bouncer": 100;
+    "Disney Princess Bouncer": 100;
+    "Dolphin Slide": 175;
+    "Elephant Bouncer": 175;
+    "YRB Slide Bouncer": 175;},
+}
+
+salesCalc: function(item, Calc) {
+
+}
