7f1038f9ccb3ef752cf3897e9b4d61b43800c159
Addey Welch <awelch@basecampcodingacademy.org>
2017-03-07 15:54:44 -0600

    Added cart to add item

--- rental.js
+++ rental.js
@@ -1,10 +1,22 @@
-var total = 0;
-var calc = {
-  'Castle Bouncer': 100,
-  'Sports Bouncer': 100,
-  'Disney Princess Bouncer': 100
+// $(document).ready(function() {
+//   function addToCart(name) {
+//     alert(name)
+//   }
+// }
+
+var Prices = {
+    Castle: 100,
+    Sports: 100
 };
 
-salesCalc: function() {
-  
+function addToCart(bouncer_name) {
+    var price = Number(Prices[bouncer_name]);
+    var total = Number($("#total").text());
+    total += price;
+    $("#total").text(total);
+    var quan_id = "#" + bouncer_name;
+    var quantity = Number($(quan_id).text());
+    quantity++;
+    $(quan_id).text(quantity);
+    alert(bouncer_name + ' Bouncer was added to the cart for $' + price);
 }
