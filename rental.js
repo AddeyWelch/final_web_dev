6aa018f33ca62d6fcafd10f6815e96e3b262668f
Addey Welch <awelch@basecampcodingacademy.org>
2017-03-08 09:40:22 -0600

    Added remove feature and added shopping cart area to page

--- rental.js
+++ rental.js
@@ -1,13 +1,10 @@
-// $(document).ready(function() {
-//   function addToCart(name) {
-//     alert(name)
-//   }
-// }
-
 var Prices = {
     Castle: 100,
     Sports: 100,
-    Princess: 100
+    Princess: 100,
+    Dolphin: 175,
+    Elephant: 175,
+    Slide: 175,
 };
 
 function addToCart(bouncer_name) {
@@ -20,4 +17,16 @@ function addToCart(bouncer_name) {
     quantity++;
     $(quan_id).text(quantity);
     alert(bouncer_name + ' Bouncer was added to the cart for $' + price);
+}
+
+function removeFromCart(bouncer_name) {
+    var price = Number(Prices[bouncer_name]);
+    var total = Number($("#total").text());
+    total -= price;
+    $("#total").text(total);
+    var quan_id = "#" + bouncer_name;
+    var quantity = Number($(quan_id).text());
+    quantity--;
+    $(quan_id).text(quantity);
+    alert(bouncer_name + ' Bouncer was removed from the cart');
 }
